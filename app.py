a9f43ba56f6d640e30b498fbe6da6b60bf211e56
ReinisSmilga <reinissmilga33@gmail.com>
2026-05-10 22:16:30 +0300

    noņēmu bootstrap un pievienoju 404.html

--- app.py
+++ app.py
@@ -18,5 +18,9 @@ def about():
 def help_page():
     return render_template("help.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
 if __name__ == "__main__":
     app.run(debug=True)
